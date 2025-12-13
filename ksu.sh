83585e09223601b280c9f1f34aa08226da606061
KongXing0819 <b170420nc@gmail.com>
2025-12-13 20:48:00 +0800

    Update ksu.sh

--- ksu.sh
+++ ksu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
