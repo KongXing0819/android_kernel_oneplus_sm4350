a0f7273f0a4c1f2d924f4eeed62b1e1bc1cf3662
KongXing0819 <b170420nc@gmail.com>
2025-12-20 05:39:08 +0800

    Update ksu.sh workflows and .config

---
+++ build/AnyKernel/anykernel.sh
@@ -0,0 +1,39 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=OnePlus Nord N200 5G Sukisu-Ultra Kernel built and packed by KongXing
+do.devicecheck=1
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name=OnePlusN200
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+### AnyKernel install
+## boot shell variables
+block=boot
+is_slot_device=auto
+ramdisk_compression=auto
+patch_vbmeta_flag=auto
+no_magisk_check=1
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh
+
+# boot install
+split_boot
+if [ -f "split_img/ramdisk.cpio" ]; then
+    unpack_ramdisk
+    write_boot
+else
+    flash_boot
+fi
+## end boot install
--- ksu.sh
+++ ksu.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
+case $1 in
+  r* | R*)
+    echo "Cloning RKSU"
+    curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
+  ;;
+  s* | S*)
+    echo "Cloning Sukisu Ultra"
+    curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
+  ;;
+  *)
+    echo "Will do Nothing"
+esac
