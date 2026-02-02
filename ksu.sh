b86005fa6bd9d268d78f09008341f2eecec85947
KongXing0819 <b170420nc@gmail.com>
2026-02-02 08:20:26 +0800

    Update ksu.sh

--- ksu.sh
+++ ksu.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-rm -r KernelSU
-case $1 in
-  r* | R*)
-    echo "Cloning RKSU"
-    curl -LSs "https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh" | bash -s main
-  ;;
-  s* | S*)
-    echo "Cloning Sukisu Ultra"
-    curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s builtin
-  ;;
-  *)
-    echo "Will do Nothing"
-esac
+NC='\033[0m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+
+if [[ -d "KernelSU" ]]; then
+    echo -e "If you want to use ReSukiSu with the ${YELLOW}RKSU Manager or others${NC},\nEnsure that ${YELLOW}CONFIG_KSU_MULTI_MANAGER_SUPPORT${NC} is ${GREEN}enabled${NC} in .config."
+fi
+#curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash -s builtin
