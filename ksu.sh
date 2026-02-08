98549f44ffdd6908730bb6fdfae40554d0d2c734
KongXing0819 <b170420nc@gmail.com>
2026-02-08 19:49:05 +0800

    Update workflows

--- ksu.sh
+++ ksu.sh
@@ -3,7 +3,7 @@ NC='\033[0m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 
-if [[ -d "KernelSU" ]]; then
+if [[ ! -d "KernelSU" ]]; then
     echo -e "If you want to use ReSukiSu with the ${YELLOW}RKSU Manager or others${NC},\nEnsure that ${YELLOW}CONFIG_KSU_MULTI_MANAGER_SUPPORT${NC} is ${GREEN}enabled${NC} in .config."
 fi
-#curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash -s builtin
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
